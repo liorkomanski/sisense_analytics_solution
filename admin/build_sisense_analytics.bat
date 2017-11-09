1a1830394ff34ff5a8236924278b786eaf81759d
Dan Kushner <dan.k0288@gmail.com>
2017-11-09 21:44:42 +0200

    Update build_sisense_analytics.bat

--- admin/build_sisense_analytics.bat
+++ admin/build_sisense_analytics.bat
@@ -1,12 +1,2 @@
-<<<<<<< HEAD
 set exe_path=C:\sisense_analytics\sisense_analytics_solution-master
 start /wait /d "%exe_path%" sisense_analytics.exe 
-=======
-@echo off
-
-set exe_path=C:\sisense_analytics\sisense_analytics_solution-master
-start /wait /d "%exe_path%" sisense_analytics.exe 
-
-
-@end
->>>>>>> origin/master
